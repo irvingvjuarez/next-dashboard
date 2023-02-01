a06609133db56f5af6b34a05213a11691e7b19c1
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-01 12:14:52 -0600

    Login page created

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,6 +1,18 @@
 import { LockClosedIcon } from '@heroicons/react/solid';
+import { FormEvent, useRef } from 'react';
 
 export default function LoginPage() {
+	const emailRef = useRef<null | HTMLInputElement>(null)
+	const passwordRef = useRef<null | HTMLInputElement>(null)
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		// console.log({
+		// 	email: emailRef.current?.value,
+		// 	password: passwordRef.current?.value
+		// })
+	}
+
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -9,20 +21,21 @@ export default function LoginPage() {
             <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
-            <form className="mt-8 space-y-6" action="#" method="POST">
+            <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
               <input type="hidden" name="remember" defaultValue="true" />
-              <div className="rounded-md shadow-sm -space-y-px">
+              <div className="rounded-md shadow-sm space-y-1">
                 <div>
                   <label htmlFor="email-address" className="sr-only">
                     Email address
                   </label>
                   <input
+										ref={emailRef}
                     id="email-address"
                     name="email"
                     type="email"
                     autoComplete="email"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Email address"
                   />
                 </div>
@@ -31,12 +44,13 @@ export default function LoginPage() {
                     Password
                   </label>
                   <input
+										ref={passwordRef}
                     id="password"
                     name="password"
                     type="password"
                     autoComplete="current-password"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Password"
                   />
                 </div>
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import LoginPage from "@/components/LoginPage";
+import Head from "next/head"
+
+export default function Login() {
+	return (
+		<>
+			<Head>
+				<title>Login</title>
+			</Head>
+
+			<LoginPage />
+		</>
+	)
+}

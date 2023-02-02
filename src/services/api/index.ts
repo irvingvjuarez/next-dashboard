fd5b584b78562aee390adbe78cfbd14fbaa58bf2
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-02 12:15:39 -0600

    Api service endpoints fulled

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,14 +1,27 @@
 const API = process.env.NEXT_PUBLIC_API_URL;
-const VERSION = process.env.NEXT_PUBLIC_API_VERSION
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 
-const endpoints = {
-	auth: {
-		login: `${API}/api/${VERSION}/auth/login`,
-		profile: `${API}/api/${VERSION}/auth/profile`
-	},
-	products: {
-		getProduct: (id: string) => `${API}/api/${VERSION}/products/${id}`
-	}
-}
+const endPoints = {
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`,
+  },
+  products: {
+    getProduct: (id: string) => `${API}/api/${VERSION}/products/${id}/`,
+    getProducts: (limit: string, offset: string) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
+    addProducts: `${API}/api/${VERSION}/products`,
+    updateProducts: (id: string) => `${API}/api/${VERSION}/products/${id}/`,
+    deleteProducts: (id: string) => `${API}/api/${VERSION}/products/${id}/`,
+  },
+  categories: {
+    getCategoriesList: `${API}/api/${VERSION}/categories/`,
+    addCategory: `${API}/api/${VERSION}/categories/`,
+    getCategoryItems: (id: string) => `${API}/api/${VERSION}/categories/${id}/products/`,
+    updateCategory: (id: string) => `${API}/api/${VERSION}/categories/${id}/`,
+  },
+  files: {
+    addImage: `${API}/api/${VERSION}/files/upload/`,
+  },
+};
 
-export default endpoints
+export default endPoints;

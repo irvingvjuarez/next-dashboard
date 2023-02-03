0b10eec9b6d4264f5911cf761805046637e06c79
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-02 22:36:03 -0600

    Trash icon added in the dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Loading from "../../common/Loading"
 import { useEffect, useState } from "react";
 import { Person } from "type";
+import { TrashIcon } from "@heroicons/react/outline"
 
 export default function Dashboard() {
 	const [people, setPeople] = useState<Person[]>([])
@@ -29,19 +30,19 @@ export default function Dashboard() {
 									<thead className="bg-gray-50">
 										<tr>
 											<th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-												Name
+												Product
 											</th>
 											<th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-												Title
+												Category
 											</th>
 											<th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-												Status
-											</th>
-											<th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-												Role
+												Stock
 											</th>
 											<th scope="col" className="relative px-6 py-3">
 												<span className="sr-only">Edit</span>
+											</th>
+											<th scope="col" className="relative px-6 py-3">
+												<span className="sr-only">Delete</span>
 											</th>
 										</tr>
 									</thead>
@@ -54,22 +55,28 @@ export default function Dashboard() {
 															<img className="h-10 w-10 rounded-full" src={person.images[0]} alt="" />
 														</div>
 														<div className="ml-4">
-															<div className="text-sm font-medium text-gray-900">{person.id}</div>
-															<div className="text-sm text-gray-500">{person.category.name}</div>
+															<div className="text-sm font-medium text-gray-900">{person.title}</div>
+															<div className="text-sm text-gray-500">${person.price}</div>
 														</div>
 													</div>
 												</td>
 												<td className="px-6 py-4 whitespace-nowrap">
-													<div className="text-sm text-gray-900">{person.title}</div>
-													<div className="text-sm text-gray-500">{person.category.name}</div>
+													<div className="text-sm text-gray-900">{person.category.name}</div>
+													{/* <div className="text-sm text-gray-500">{person.category.id}</div> */}
 												</td>
 												<td className="px-6 py-4 whitespace-nowrap">
-													<span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+													<span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+														{Math.round(Math.random() * 1000)}
+													</span>
 												</td>
-												<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.description}</td>
 												<td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
 													<a href="#" className="text-indigo-600 hover:text-indigo-900">
 														Edit
+													</a>
+												</td>
+												<td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+													<a href="#" className="bg-red-400 hover:text-indigo-900">
+														<TrashIcon className="text-red-500" />
 													</a>
 												</td>
 											</tr>
